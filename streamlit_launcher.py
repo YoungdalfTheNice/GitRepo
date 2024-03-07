d4b93fc345f261d86d55e0a4dfd991bde3ff0681
Jannis Gruß <113449329+YoungdalfTheNice@users.noreply.github.com>
2024-03-07 12:14:59 +0100

    Delete streamlit_launcher.py

--- streamlit_launcher.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-from setup_yt_channel_transcripts import zielverzeichnis
-
-script_path = os.path.join(zielverzeichnis, "streamlit.py")
-
-subprocess.run(['streamlit', 'run', script_path])
